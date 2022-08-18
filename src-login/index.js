196f96534bd50a51d8ba8820c6b4f0bb68e1cdac
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2022-08-18 23:52:37 +0530

    New Changes again!!!!!!!

--- src-login/index.js
+++ src-login/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 const txtEmail = document.querySelector('#txtemail')
 const txtPassword = document.querySelector('#txtpassword')
