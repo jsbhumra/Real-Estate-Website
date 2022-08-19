da660f82747254f06e816f7d79a64bd62bb89a92
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2022-08-19 12:50:38 +0530

    Fixing errors

--- src-login/index.js
+++ src-login/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 
 const txtEmail = document.querySelector('#txtemail')
 const txtPassword = document.querySelector('#txtpassword')
